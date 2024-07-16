9843a212b4dea8cd369a3c150873f1baa701d849
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2024-07-16 08:56:41 -0500

    Create variables.tf

---
+++ addDatabricksSubnets/variables.tf
@@ -0,0 +1,31 @@
+variable "int-databricks-subnet-name" {
+    type = string
+}
+
+variable "ext-databricks-subnet-name" {
+    type = string
+}
+
+variable "name-of-vnet-resourcegroup" {
+    type = string
+}
+
+variable "location-of-vnet" {
+    type = string
+}
+
+variable "databricks-nsg-name" {
+    type = string
+}
+
+variable "int-subnet-cidr" {
+    type = string
+}
+  
+variable "ext-subnet-cidr" {
+    type = string
+}
+
+variable "name-of-vnet" {
+    type = string
+}
